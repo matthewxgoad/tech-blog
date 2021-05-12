8838ef04746ba1455592884f510743af155db4a5
Matthew Goad <matthewxgoad@gmail.com>
2021-05-12 13:21:51 -0700

    updated Models and routes

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,5 +1,5 @@
 const sequelize = require('../config/connection');
-const { User } = require('../models');
+const { User, Blog, Comment } = require('../models');
 
 const userData = require('./userData.json');
 
@@ -7,6 +7,14 @@ const seedDatabase = async () => {
   await sequelize.sync({ force: true });
 
   await User.bulkCreate(userData, {
+    individualHooks: true,
+    returning: true,
+  });
+  await Blog.bulkCreate(blogData, {
+    individualHooks: true,
+    returning: true,
+  });
+  await Comment.bulkCreate(commentData, {
     individualHooks: true,
     returning: true,
   });
